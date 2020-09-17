83b2020e59ff1cbdef57783ab54b3b02f54f5075
Heder Felipe <hederfelipe@gmail.com>
2020-09-17 13:14:00 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
+
 
 
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <div>
     <Header />
+    <Home />
     </div>
   );
 }
--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles.css';
-import { ReactComponent as Vector } from '../../assets/Vector.svg';
+import { ReactComponent as Logo } from '../../assets/Logo.svg';
 
 const Header =()=>(
    <header className="main-header">
-       <Vector />
+       <Logo />
        <div className="logo-text">
            <span className="logo-text-1">Big Game</span>
            <span className="logo-text-2"> Survey</span>
